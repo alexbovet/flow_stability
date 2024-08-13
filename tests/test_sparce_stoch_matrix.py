6cdb9889603d1feab49d304732fc31cb7edd0539
Jonas I. Liechti <j-i-l@t4d.ch>
2024-08-13 15:57:29 +0200

    adding first test script

---
+++ tests/test_sparce_stoch_matrix.py
@@ -0,0 +1,10 @@
+def test_SSM():
+    """Bacic operations with the 'spares_stoch_mat' class
+    """
+    from flowstab.SparseStochMat import sparse_stoch_mat
+    # TODO
+
+def test_SPA():
+    """Basic operations with the `SpasreStochMat.SPA` class"""
+    from flowstab.SparseStochMat import SPA
+    # TODO
