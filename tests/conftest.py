a350bfd6bf64fc65b5b32f4944f3f0b3601a8145
Jonas I. Liechti <j-i-l@t4d.ch>
2024-08-20 23:37:04 +0200

    speed and memory checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,22 @@ import numpy as np
 from scipy.sparse import csr_matrix
 
 @pytest.fixture(scope='function')
-def get_csr_matrix():
+def get_csr_matrix_small():
     """Creat an exemplary csr matrix that can be used for testing
     """
     row = np.array([0, 0, 1, 2, 3, 3])
     col = np.array([0, 2, 2, 1, 3, 4])
     data = np.array([1, 2, 3, 5, 1, 6])
     return csr_matrix((data, (row, col)), shape=(5, 5))
+
+@pytest.fixture(scope='function')
+def get_csr_matrix_large():
+    """Creat an exemplary csr matrix that can be used for testing
+    """
+    size = 10000
+    nbr_non_zeros = 1000
+    row = np.sort(np.random.randint(0, size, size=nbr_non_zeros))
+    col = np.sort(np.random.randint(0, size, size=nbr_non_zeros))
+    data = np.random.randint(0,100, size=nbr_non_zeros)
+    density = nbr_non_zeros / size
+    return csr_matrix((data, (row, col)), shape=(size, size)), density
--- tests/test_sparse_stoch_matrix.py
+++ tests/test_sparse_stoch_matrix.py
@@ -1,19 +1,36 @@
 import numpy as np
-from scipy.sparse import csr_matrix, coo_matrix
+import tracemalloc
 
-def test_SSM(get_csr_matrix):
+def test_SSM_small(get_csr_matrix_small):
     """Bacic operations with the 'spares_stoch_mat' class
     """
     from flowstab.SparseStochMat import sparse_stoch_mat as SSM
     # Inits
     # ###
     # inti from scipy.sparse.csr_matrix
-    A_csr = get_csr_matrix.copy()
-    ssm = SSM.from_full_csr_matrix(A_csr)
-    np.testing.assert_equal(A_csr.toarray(), ssm.toarray(), strict=False)
-    
+    A_csr = get_csr_matrix_small
+    for i in range(10000):
+        ssm = SSM.from_full_csr_matrix(A_csr)
+        np.testing.assert_equal(A_csr.toarray(), ssm.toarray(), strict=False)
     
 
+def test_SSM_large(get_csr_matrix_large):
+    """Make sure an SSM does not get expanded during creation
+    """
+    from flowstab.SparseStochMat import sparse_stoch_mat as SSM
+    # Inits
+    # ###
+    # inti from scipy.sparse.csr_matrix
+    A_csr, density = get_csr_matrix_large
+    tracemalloc.start()
+    for i in range(100):
+        _ = SSM.from_full_csr_matrix(A_csr)
+    first_size, first_peak = tracemalloc.get_traced_memory()
+    for i in range(100):
+        _ = SSM.from_full_csr_matrix(A_csr).toarray()
+    second_size, second_peak = tracemalloc.get_traced_memory()
+    assert first_size < density * second_size
+    assert first_peak < density * second_peak
 
 
 
