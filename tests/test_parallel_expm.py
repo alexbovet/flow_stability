043c898d872d77497ce687795f76313fc6cc4a5b
Jonas I. Liechti <j-i-l@t4d.ch>
2024-09-29 10:42:51 +0200

    init tests for parallel_expm

---
+++ tests/test_parallel_expm.py
@@ -0,0 +1,24 @@
+import pytest
+
+import numpy as np
+
+from flowstab.parallel_clustering import (
+    # _init_worker,
+    # _worker,
+    compute_parallel_expm,
+    # _stack_sparse_cols,
+    # _expm_worke,
+    compute_subspace_expm_parallel
+)
+
+def test_compute_parallel_expm():
+    """
+    """
+    from flowstab.parallel_clustering import compute_parallel_expm
+    pass
+
+def test_compute_subspace_parallel_expm():
+    """
+    """
+    from flowstab.parallel_clustering import compute_subspace_expm_parallel
+    pass
