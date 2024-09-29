db1fecc4809395dee045d8d1a8d20f2d2f2b25d8
Jonas I. Liechti <j-i-l@t4d.ch>
2024-09-29 11:14:19 +0200

    init tests for temporal_network

---
+++ tests/test_temporal_network.py
@@ -0,0 +1,85 @@
+import pytest
+
+def test_ContTempNetwork():
+    """
+    """
+    from flowstab.TemporalNetwork import ContTempNetwork
+    pass
+
+def test_ContTempInstNetwork():
+    """
+    """
+    from flowstab.TemporalNetwork import ContTempInstNetwork
+    pass
+
+def test_lin_approx_trans_matrix():
+    """
+    """
+    from flowstab.TemporalNetwork import lin_approx_trans_matrix
+    pass
+
+def test_compute_stationary_transition():
+    """
+    """
+    from flowstab.TemporalNetwork import compute_stationary_transition
+    pass
+
+def test_compute_subspace_expm():
+    """
+    """
+    from flowstab.TemporalNetwork import compute_subspace_expm
+    pass
+
+def test_csc_row_normalize():
+    """
+    """
+    from flowstab.TemporalNetwork import csc_row_normalize
+    pass
+
+def test_find_spectral_gap():
+    """
+    """
+    from flowstab.TemporalNetwork import find_spectral_gap
+    pass
+
+def test_remove_nnz_rowcol():
+    """
+    """
+    from flowstab.TemporalNetwork import remove_empty_clusters
+    pass
+
+def test_rebuild_nnz_rowcol():
+    """
+    """
+    from flowstab.TemporalNetwork import numpy_rebuild_nnz_rowcol
+    pass
+
+def test_sparse_lapl_expm():
+    """
+    """
+    from flowstab.TemporalNetwork import sparse_lapl_expm
+    pass
+
+def test_sparse_lin_approx():
+    """
+    """
+    from flowstab.TemporalNetwork import sparse_lin_approx
+    pass
+
+def test_sparse_stationary_trans():
+    """
+    """
+    from flowstab.TemporalNetwork import sparse_stationary_trans
+    pass
+
+def test_set_to_ones():
+    """
+    """
+    from flowstab.TemporalNetwork import set_to_ones
+    pass
+
+def test_set_to_zeroes():
+    """
+    """
+    from flowstab.TemporalNetwork import set_to_zeroes
+    pass
