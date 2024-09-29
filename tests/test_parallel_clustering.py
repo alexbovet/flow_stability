decc937780bad170a2fc2d9050bdbe80ce1a0cf0
Jonas I. Liechti <j-i-l@t4d.ch>
2024-09-29 11:25:19 +0200

    init tests for parallel_clustering

---
+++ tests/test_parallel_clustering.py
@@ -0,0 +1,19 @@
+import pytest
+
+def test_compute_parallel_clustering():
+    """
+    """
+    from flowstab.parallel_clustering import compute_parallel_clustering
+    pass
+
+def test_n_random_seeds():
+    """
+    """
+    from flowstab.parallel_clustering import n_random_seeds
+    pass
+
+def test_compute_parallel_nvi():
+    """
+    """
+    from flowstab.parallel_clustering import compute_parallel_nvi
+    pass
