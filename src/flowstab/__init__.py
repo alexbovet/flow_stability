32936d26cc5e8e7b514cb058154c27b6b1d068b2
Jonas I. Liechti <j-i-l@t4d.ch>
2025-05-21 18:29:28 +0200

    better formatting

--- src/flowstab/__init__.py
+++ src/flowstab/__init__.py
@@ -1,3 +1,23 @@
+"""
+flow stability
+
+Copyright (C) 2021 Alexandre Bovet <alexandre.bovet@maths.ox.ac.uk>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
 import logging
 
 from .flow_stability import FlowStability
@@ -10,8 +30,10 @@ def set_log_level(level):
     """
     Set the logging level for the package.
 
-    Args:
-        level (str): The logging level as a string (e.g., 'DEBUG', 'INFO').
+    Parameters
+    ----------
+    level : str
+        The logging level as a string (e.g., 'DEBUG', 'INFO').
     """
     level_dict = {
         'DEBUG': logging.DEBUG,
