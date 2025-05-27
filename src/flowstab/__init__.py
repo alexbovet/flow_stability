b243d5177c0cf927383057329fae01252d1ddd1b
Jonas I. Liechti <j-i-l@t4d.ch>
2025-05-27 16:21:52 +0200

    docstring for module

--- src/flowstab/__init__.py
+++ src/flowstab/__init__.py
@@ -1,21 +1,64 @@
 """
-flow stability
+Flow Stability: Temporal Network Flow-Based Clustering and Analysis
+==================================================================
 
-Copyright (C) 2021 Alexandre Bovet <alexandre.bovet@maths.ox.ac.uk>
+This package provides tools for stability analysis and clustering of temporal networks
+using flow-based methods. The core component is the :class:`FlowStability` class,
+which implements a reproducible, state-tracked analytical pipeline for temporal (contact sequence)
+data. The package also includes logging utilities to facilitate debugging and reproducible research.
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation; either version 3 of the License, or (at your option) any
-later version.
+Main Features
+-------------
+- Load and process temporal networks (contact sequences) from files or arrays.
+- Compute Laplacian and inter-transition matrices for random walks on temporal networks.
+- Extract flow-based clusterings using methods such as the Louvain algorithm.
+- Track analysis progress and enforce correct computational order through a robust state machine.
+- Configurable logging for both development and production.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-details.
+Logging
+-------
+The package sets up a default logger on import. You can adjust the logging level:
 
-You should have received a copy of the GNU Lesser General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+>>> import flowstab
+>>> flowstab.set_log_level("DEBUG")
 
+Usage Example
+-------------
+A typical workflow for analyzing a temporal network and extracting clusters:
+
+>>> from flowstab import FlowStability
+>>> fs = FlowStability()
+>>> fs.set_temporal_network(filename="my_contacts.csv")
+>>> fs.set_time_scale(10)
+>>> fs.compute_laplacian_matrices()
+>>> fs.compute_inter_transition_matrices()
+>>> fs.set_flow_clustering()
+>>> fs.find_louvain_clustering()
+>>> print(fs.flow_clustering_forward)
+
+This workflow ensures that all computational steps are performed in the required order;
+the state machine will warn or prevent you from skipping prerequisites.
+
+Module Contents
+---------------
+- FlowStability
+    Main class for performing flow stability analysis and clustering.
+- set_log_level
+    Function to set the global logging level for the package.
+
+Author
+------
+Alexandre Bovet <alexandre.bovet@maths.ox.ac.uk>
+
+
+Contributors
+............
+
+- Jonas I. Liechti <j-i-l@t4d.ch>
+
+License
+-------
+GNU Lesser General Public License v3 or later (LGPLv3+).
 
 """
 import logging
